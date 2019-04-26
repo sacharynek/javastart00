2513343e434480b6d9d044bcecb5ae24fe70d486
tomaszlach86 <tomaszlach86@gmail.com>
2019-04-26 19:29:13 +0200

    First Commit

---
+++ src/main/java/HelloBitches.java
@@ -0,0 +1,13 @@
+public class HelloBitches {
+
+    public static void main(String[] args) {
+        System.out.println("Hello bitches");
+
+        //sout
+        System.out.println("bla bla bla");
+        //ctrl+spacja - wymusza pomoc
+
+        //ctrl+d - duplikuje wiersz
+    }
+
+}
